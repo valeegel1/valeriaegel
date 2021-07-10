fd8ff2b2de0d02052baedb11cca139f87d554f15
Valeria <valeriaegel@gmial.com>
2021-07-09 21:34:02 -0300

    inicial

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function click1 (){
+    const click1 = document.getElementById("principal");
+    click1.scrollIntoView();
+}
+
+function click2 (){
+    const click2 = document.getElementById("seccion1");
+    click2.scrollIntoView();
+}
+
+function click3 (){
+    const click3 = document.getElementById("contacto");
+    click3.scrollIntoView();
+}
+
+function github() {
+    window.open( "https://github.com/valeegel1");
+}
+
+function gmail() {
+    window.open("https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSBmlkBlbWjVsvSvkSDrcwKvxQnJcKSlTPclhQRNXSfCjtDdFgHnfmMnqrfbvwFkwhbFNNvX");
+}
+
+function instagram() {
+  window.open( "https://www.instagram.com/valeegel");
+}
